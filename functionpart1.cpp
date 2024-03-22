796d766a97436ed66c043f7d97b920ebfe7befab
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-03-22 20:34:46 +0600

    Add files via upload

---
+++ functionpart1.cpp
@@ -0,0 +1,16 @@
+//user input function calling
+#include<iostream>
+using namespace std;
+
+void hell(string name){
+
+cout<<"hello "<<name;
+
+           }
+int main(){
+string n;
+cin>>n;
+hell(n);
+return 0 ;
+
+}
