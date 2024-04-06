ab020918fc7c50c612151c1fefc31f7f2adce7f0
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-04-06 05:42:25 -0800

    Create ceiling.cpp

---
+++ ceiling.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    float a, b;
+   // cin>>a;
+   // cin>>b;
+   // float sum =a/b;
+    cout<<ceil(3.4)<<endl;
+  cout<<floor(3.4);
+  return 0 ;
+}
